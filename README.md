d24e158492263bcb48cc32a3ab3455cf7f2e74b3
Mywk <mywk@msn.com>
2013-11-01 16:30:36 +0000

    Initial Release

---
+++ README.md
@@ -0,0 +1,7 @@
+LogBlock
+==========
+
+Modified version of LogBlock.
+Improved compatibility with Spigot/MCPC + Extended ID Logging & Improved efficiency
+
+Questioner: http://git.io/u2MxKQ
---
+++ src/main/java/de/diddiz/LogBlock/listeners/ChestAccessLogging.java
@@ -0,0 +1,106 @@
+package de.diddiz.LogBlock.listeners;
+
+import static de.diddiz.LogBlock.config.Config.isLogging;
+import static de.diddiz.util.BukkitUtils.compareInventories;
+import static de.diddiz.util.BukkitUtils.compressInventory;
+import static de.diddiz.util.BukkitUtils.getInventoryHolderLocation;
+import static de.diddiz.util.BukkitUtils.getInventoryHolderType;
+import static de.diddiz.util.BukkitUtils.rawData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import de.diddiz.LogBlock.Logging;
+
+import org.bukkit.Location;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.DoubleChest;
+import org.bukkit.entity.HumanEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+import de.diddiz.LogBlock.LogBlock;
+import de.diddiz.LogBlock.config.Config;
+
+public class ChestAccessLogging extends LoggingListener
+{
+	private final Map<HumanEntity, ItemStack[]> containers = new HashMap<HumanEntity, ItemStack[]>();
+
+	public ChestAccessLogging(LogBlock lb) {
+		super(lb);
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onInventoryClose(InventoryCloseEvent event) {
+
+		if (!isLogging(event.getPlayer().getWorld(), Logging.CHESTACCESS)) return;
+		InventoryHolder holder = event.getInventory().getHolder();
+		// Nop, screw this line! just log all machines with an inventory :)
+		//if (holder instanceof BlockState || holder instanceof DoubleChest) 
+		{
+			final HumanEntity player = event.getPlayer();
+			final ItemStack[] before = containers.get(player);
+			if (before != null) {
+				final ItemStack[] after = compressInventory(event.getInventory().getContents());
+				final ItemStack[] diff = compareInventories(before, after);
+				
+				// Huge problem here, some machines with inventory may not report their
+				// location correctly so instead of fixing every single of these mods
+				// we will just add ChestAccess to the block the player is looking at.
+				// (HotFix also implemented in BukkitUtils.java)
+								
+				Location loc = getInventoryHolderLocation(holder);
+				int itemTypeId = 0;
+				if (!(holder instanceof BlockState || holder instanceof DoubleChest) && Config.getCustomBlockIds().contains(event.getPlayer().getTargetBlock(null,200).getTypeId()))
+				{
+					loc = event.getPlayer().getTargetBlock(null,200).getLocation();
+				}
+				
+				itemTypeId = loc.getWorld().getBlockTypeIdAt(loc);
+				//System.out.println("itemTypeId" + itemTypeId);		
+				//System.out.println("data" + loc.getWorld().getBlockAt(loc).getData());		
+				// Satefy check
+				if(itemTypeId != 0 && loc != null)
+				{
+					for (ItemStack item : diff) {
+						  //System.out.println(player.getName());
+						  //System.out.println("Loc:" + loc.getX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ());
+						  //System.out.println("itemTypeId" + itemTypeId);
+						  //System.out.println(item.getAmount());
+						  //System.out.println(rawData(item));
+						  this.consumer.queueChestAccess(player.getName(), loc, itemTypeId, (short)item.getTypeId(), (short)item.getAmount(), rawData(item));
+				    }
+				}
+				containers.remove(player);
+			}
+		}
+	}
+
+	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+	public void onInventoryOpen(InventoryOpenEvent event) {
+		if (!isLogging(event.getPlayer().getWorld(), Logging.CHESTACCESS)) return;
+		if (event.getInventory() != null) {
+			InventoryHolder holder = event.getInventory().getHolder();
+
+			// Screw this too.
+			if (holder instanceof BlockState || holder instanceof DoubleChest || Config.getCustomBlockIds().contains(event.getPlayer().getTargetBlock(null,200).getTypeId())) 
+			{
+				//test
+				//java.util.ListIterator<ItemStack> x = event.getInventory().iterator();
+				//for(ItemStack y : x)
+				//{
+					
+				//}
+				//end test
+				if (getInventoryHolderType(holder) != 58) {
+					containers.put(event.getPlayer(), compressInventory(event.getInventory().getContents()));
+				}
+			}
+		}
+	}
+}
